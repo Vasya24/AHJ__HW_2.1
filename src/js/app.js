ae34006dfc1694f2809adbe3981cced79de4ef20
Василий Смирнов <vasya24@MacBook-Air-Vasilij.local>
2020-06-25 11:22:10 +0300

    Done

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,23 @@
-console.log('Работает!')
+(() => {
+    let playing = true,
+      activeField = 1;
+  
+    const stop = () => playing = true,
+      getField = index => document.getElementById(`field_${index}`),
+      deactivateField = index =>
+        getField( index ).className = 'field',
+      activateField = index =>
+        getField( index ).className = 'field has_goblin',
+      next = () => setTimeout(() => {
+        if ( !playing ) {
+          return;
+        }
+        deactivateField( activeField );
+        activeField = Math.floor( 1 + Math.random() * 16 );
+        activateField( activeField );
+        next();
+      }, 800 );
+  
+    next();
 
+  })();
